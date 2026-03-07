859840ae42c4997cae017313e2de5b6d8740ea18
fabioangeletti-Italy <fabio.angeletti@ipsiaterni.edu.it>
2026-03-07 21:36:15 +0100

    Update script.js
    Signed-off-by: fabioangeletti-Italy <fabio.angeletti@ipsiaterni.edu.it>

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const myBibleVerses = [
-"John 8:12 — I am the light of the world",
+"John 8:12 I am the light of the world",
 "The Lord is my shepherd; I shall not want","He makes me lie down in green pastures He leads me beside quiet waters",
 "He restores my soul. He guides me in paths of righteousness for His name’s sake","Even though I walk through the valley of the shadow of death, I will fear no evil, for You are with me; Your rod and Your staff, they comfort me",
 "You prepare a table before me in the presence of my enemies. You anoint my head with oil; my cup overflows",
 "Surely goodness and mercy shall follow me all the days of my life, and I will dwell in the house of the Lord forever",
-"Philippians 4:13 — I can do all things through Christ who strengthens me",
-"Proverbs 3:5 — Trust in the Lord with all your heart"
+"Philippians 4:13 I can do all things through Christ who strengthens me",
+"Proverbs 3:5 Trust in the Lord with all your heart"
 ];
 
 function getNaturalSpeed(char){
