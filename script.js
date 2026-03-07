409383b25d10dfc933434ae9a3a50dc357990b90
fabioangeletti-Italy <fabio.angeletti@ipsiaterni.edu.it>
2026-03-07 16:17:12 +0100

    Update script.js
    Signed-off-by: fabioangeletti-Italy <fabio.angeletti@ipsiaterni.edu.it>

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+async function loadVerses(){
+const response = await fetch('verses.json');
+return await response.json();
+}
 
+async function showVerse(){
+const verses = await loadVerses();
+const randomVerse = verses[Math.floor(Math.random() * verses.length)];
+
+const verseBox = document.getElementById("verseBox");
+
+// Dissolvenza uscita
+verseBox.style.transition = "opacity 1.2s ease, transform 1.2s ease";
+verseBox.style.opacity = "0";
+verseBox.style.transform = "translateY(10px)";
+
+setTimeout(() => {
+
+    // Cambia testo
+    verseBox.innerText = randomVerse;
+
+    // Dissolvenza entrata
+    verseBox.style.opacity = "1";
+    verseBox.style.transform = "translateY(0px)";
+
+}, 1200);
+
+
+}
+
+function startAutoVerseRotation(){
+showVerse();
+setInterval(showVerse, 300000); // 5 minuti
+}
+
+document.addEventListener("DOMContentLoaded", startAutoVerseRotation);
